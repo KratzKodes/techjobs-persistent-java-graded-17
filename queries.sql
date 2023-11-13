f5f54f8fef9aa06cc1b5887c88217006a8063797
Maurice Kratz <kratzkodes@gmail.com>
2023-11-12 21:33:28 -0500

    Updating TaskTwo

--- queries.sql
+++ queries.sql
@@ -1,7 +1,14 @@
 --Part 1
 
---Part 2
+--id int PK
+--employer varchar(255)
+--name varchar(255)
+--skills varchar(255)
+--employer_id int
+--jobs_id int
 
+--Part 2
+ SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 4000)
+    private String description;
+
+    public Skill(){}
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
